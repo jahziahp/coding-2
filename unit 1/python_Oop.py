d5deb06f831be647a9333e970af81cb1dd3b4533
jahziahp <petersj255@boyslatin.net>
2026-02-25 15:23:19 +0000

    bank system class work

---
+++ unit 1/python_Oop.py
@@ -0,0 +1,21 @@
+# Python OOP - object oreinted programing
+
+# object - A represenation 
+# of some complex data structure 
+# *data types (... and functions in this case)
+# Although they maybe the same thing, objectively, onjects
+# can different characteristics / featuers
+
+# class - is a constructor / blueprint for creating
+# objects
+
+class computers:
+    def _init_(self, color, shape, storage, protability, camera, ram,):
+        self.color = color'
+        self.shape = shape
+        self.storage = storage
+        self.protability = protability
+        self.camera = camera
+        self.ram = ram
+
+    apple_1 = computers("apple m4", 'black', 10.00, true, true, 60, 'm4')
