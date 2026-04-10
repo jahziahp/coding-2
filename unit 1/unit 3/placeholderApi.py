d8e0c1f4ce72a6997726ec4afdc058e50c96413a
jahziahp <petersj255@boyslatin.net>
2026-04-10 14:26:40 +0000

    placeholderApi

---
+++ unit 1/unit 3/placeholderApi.py
@@ -0,0 +1,27 @@
+#import requests
+
+#query = 'https://jsonplaceholder.typicode.com/todos/1'
+
+#response = requests.get(query)
+
+#print(response)
+#print(response.json()
+
+
+#import requests
+
+#url = 'https://bored-api.appbrewery.com/random'
+
+#response = requests.get(url)
+
+#print(response)
+#print(response)
+
+
+import requests
+
+query = 'https://pokeapi.co/api/v2/berry/45'
+response = requests.get(query)
+
+print(response)
+print(response.json())
